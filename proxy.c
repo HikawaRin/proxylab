d05628183f1d0a59ba262d4254a7b9326d8cf5bd
Hikawa Rin <HikawaRin@outlook.com>
2022-05-08 18:03:42 +0800

    add parse_uri method

--- proxy.c
+++ proxy.c
@@ -13,7 +13,7 @@ void Transaction(int fd);
 int main(int argc, char **argv) { 
   int listenfd, connfd;
   socklen_t clientlen;
-  struct sockaddr_storage clientaddrs;
+  struct sockaddr_storage clientaddr;
 
   if (argc != 2) { 
     fprintf(stderr, "usage: %s <port>\n", argv[0]);
@@ -24,7 +24,7 @@ int main(int argc, char **argv) {
     clientlen = sizeof(clientaddr);
     connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
     
-    
+    Transaction(connfd); 
   }
 
   return 0;
@@ -61,4 +61,61 @@ void proxy_error(int fd, char *cause, char *errnum,
   Rio_writen(fd, buf, strlen(buf));
 
   Close(fd);
+}
+
+// parse_uri - parse URI into scheme://<host>:<port><abs_path>
+//              return 0 if success, 1 if failure
+int parse_uri(const char *uri, char *host, char *port, char *abs_path) { 
+  char *it = strstr(uri, "://");
+  if (it != NULL) { 
+    // an absoluteURI
+    it += 3;
+    char *end = strstr(it, "/");
+    if (end == NULL) { strncpy(host, it, MAXLINE); }
+    else { strncpy(host, it, (size_t)(end-it)); }
+
+    it = strstr(host, ":");
+    if (it != NULL) { 
+      strncpy(port, it+1, MAXLINE); 
+      *it = '\0';
+    }
+
+    it = end;
+  } else { 
+    it = strstr(uri, "/"); 
+    if (it == NULL) { return 1; }
+  }
+
+  // port not specified, default 80
+  if (!strcmp(port, "")) { strncpy(port, "80\0", 3); }
+
+  if (it == NULL) { strncpy(abs_path, "/\0", 2); }
+  else { strncpy(abs_path, it, MAXLINE); }
+
+  return 0;
+}
+
+void Transaction(int fd) { 
+  rio_t rio;
+  char buf[MAXLINE]; 
+  char method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  char host[MAXLINE], port[MAXLINE], abs_path[MAXLINE];
+
+  Rio_readinitb(&rio, fd);
+  // read Request-Line
+  if (!Rio_readlineb(&rio, buf, MAXLINE)) { 
+    Close(fd); return;
+  }
+  sscanf(buf, "%s %s %s", method, uri, version); // parse Request-Line
+  if (strcasecmp(method, "GET")) { 
+    proxy_error(fd, method, "501", 
+        "Not Implemented", "Hikawa Proxy does not implement this method. ");
+    return;
+  }
+  if (parse_uri(uri, host, port, abs_path)) { 
+    proxy_error(fd, method, "400", 
+        "Bad Request", "Request-URI broken. ");
+    return; 
+  }
+
 }
